c04daadfb8a66c042da54b82ed205a73ac1197c9
yonding <uwrgoy7584@khu.ac.kr>
2023-11-07 17:28:32 +0900

    Feat: POST /api/verifyIamport 구현 #22

--- src/main/java/shop/mulmagi/app/domain/enums/PaymentMethod.java
+++ src/main/java/shop/mulmagi/app/domain/enums/PaymentMethod.java
@@ -2,9 +2,9 @@ package shop.mulmagi.app.domain.enums;
 
 public enum PaymentMethod {
     KAKAOPAY("카카오페이"),
+    TOSSPAY("토스페이"),
     NAVERPAY("네이버페이"),
     APPLEPAY("애플페이"),
-    TOSS("토스"),
     ETC("기타");
 
     private final String paymentMethod;
