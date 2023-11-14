09d054763f769a35a045e2831b9e9d80c59451a0
Yuseunggeun <yuu3730@naver.com>
2023-11-14 20:24:12 +0900

    Update: Add swagger annotation

--- src/main/java/shop/mulmagi/app/web/dto/AnnouncementRequestDto.java
+++ src/main/java/shop/mulmagi/app/web/dto/AnnouncementRequestDto.java
@@ -1,8 +1,5 @@
 package shop.mulmagi.app.web.dto;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.annotations.ApiModelProperty;
@@ -16,36 +13,45 @@ public class AnnouncementRequestDto {
 	@Getter @Setter
 	public static class UploadDto{
 		@ApiModelProperty(example = "이용수칙 개정 공지사항")
-		@ApiParam(name = "umbrellaStandId", value = "Umbrella Stand ID 입력", required = true)
+		@ApiParam(name = "title", value = "공지사항 제목 입력")
 		private String title;
 
 		@ApiModelProperty(example = "NORMAL")
+		@ApiParam(name = "category", value = "공지사항 카테고리 입력")
 		private AnnouncementCategory category;
 
 		@ApiModelProperty(example = "이용수칙 개정 안내드립니다")
+		@ApiParam(name = "content", value = "공지사항 내용 입력")
 		private String content;
 
 		@ApiModelProperty(example = "a.png")
+		@ApiParam(name = "photo", value = "이미지 파일 선택")
 		private MultipartFile photo;
 
 		@ApiModelProperty(example = "b.txt")
+		@ApiParam(name = "file", value = "파일 선택")
 		private MultipartFile file_0;
 	}
 	@Getter @Setter
 	public static class UpdateDto{
 		@ApiModelProperty(example = "이용수칙 개정 공지사항")
+		@ApiParam(name = "title", value = "공지사항 제목 입력")
 		private String title;
 
 		@ApiModelProperty(example = "NORMAL")
+		@ApiParam(name = "category", value = "공지사항 카테고리 입력")
 		private AnnouncementCategory category;
 
 		@ApiModelProperty(example = "이용수칙 개정 안내드립니다")
+		@ApiParam(name = "content", value = "공지사항 내용 입력")
 		private String content;
 
 		@ApiModelProperty(example = "a.png")
+		@ApiParam(name = "photo", value = "이미지 파일 선택")
 		private MultipartFile photo;
 
 		@ApiModelProperty(example = "b.txt")
+		@ApiParam(name = "file", value = "파일 선택")
 		private MultipartFile file_0;
 	}
 }
