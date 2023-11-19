8b733b46162b7a7335f61aeef5eb0ca09801ce08
yonding <uwrgoy7584@khu.ac.kr>
2023-11-19 13:41:26 +0900

    Fix: ResponseMessage 중복 제거

--- src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
+++ src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
@@ -29,17 +29,5 @@ public class ResponseMessage {
     //sms certification
     public static final String SMS_CERT_SUCCESS = "sms 본인 확인 성공";
     public static final String SMS_CERT_MESSAGE_SUCCESS = "인증 문자 전송 성공";
-
-    //notice
-    public static final String NOTICE_READ_SUCCESS = "공지사항 불러오기 성공";
-    public static final String NOTICE_DETAIL_READ_SUCCESS = "공지사항 세부내용 불러오기 성공";
-    public static final String NOTICE_CATEGORY_READ_SUCCESS = "공지사항 카테고리 불러오기 성공";
-    public static final String NOTICE_UPLOAD_SUCCESS = "공지사항 업로드 성공";
-    public static final String NOTICE_DELETE_SUCCESS = "공지사항 삭제 성공";
-    public static final String NOTICE_UPDATE_SUCCESS = "공지사항 수정 성공";
-
-    //report
-    public static final String REPORT_RECEIVE_SUCCESS = "고장신고 접수 성공";
-    public static final String REPORT_SOLVE_SUCCESS = "고장신고 해결처리 성공";
   
 }
