874c2f8ab7b51c8e37522761b57894b20ce467bb
eqvyoo <ys07106@gmail.com>
2023-11-19 12:15:06 +0900

    Update: added responsemessage to resolve conflicts

--- src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
+++ src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
@@ -12,9 +12,21 @@ public class ResponseMessage {
     public static final String RENTAL_SUCCESS = "대여 성공";
     public static final String RETURN_SUCCESS = "반납 성공";
     public static final String PAYMENT_SUCCESS = "결제 성공";
+    //notice
+    public static final String NOTICE_READ_SUCCESS = "공지사항 불러오기 성공";
+    public static final String NOTICE_DETAIL_READ_SUCCESS = "공지사항 세부내용 불러오기 성공";
+    public static final String NOTICE_CATEGORY_READ_SUCCESS = "공지사항 카테고리 불러오기 성공";
+    public static final String NOTICE_UPLOAD_SUCCESS = "공지사항 업로드 성공";
+    public static final String NOTICE_DELETE_SUCCESS = "공지사항 삭제 성공";
+    public static final String NOTICE_UPDATE_SUCCESS = "공지사항 수정 성공";
+
+    //report
+    public static final String REPORT_RECEIVE_SUCCESS = "고장신고 접수 성공";
+    public static final String REPORT_SOLVE_SUCCESS = "고장신고 해결처리 성공";
 
     //sms certification
     public static final String SMS_CERT_SUCCESS = "sms 본인 확인 성공";
     public static final String SMS_CERT_MESSAGE_SUCCESS = "인증 문자 전송 성공";
+
 
 }
