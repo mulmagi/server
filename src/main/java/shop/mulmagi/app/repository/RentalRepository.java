3f2036c05ccadc1d1b83c0e10cbea451009c1859
Yuseunggeun <yuu3730@naver.com>
2023-11-19 19:24:30 +0900

    Feat: 고장신고 기능 수정 #69

--- src/main/java/shop/mulmagi/app/repository/RentalRepository.java
+++ src/main/java/shop/mulmagi/app/repository/RentalRepository.java
@@ -2,6 +2,7 @@ package shop.mulmagi.app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.mulmagi.app.domain.Rental;
+import shop.mulmagi.app.domain.UmbrellaStand;
 import shop.mulmagi.app.domain.User;
 
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface RentalRepository extends JpaRepository<Rental, Long> {
     List<Rental> findByIsReturn(Boolean isReturn);
     Rental findByUserAndIsReturn(User user, Boolean isReturn);
+    Rental findByReturnUmbrellaStand(UmbrellaStand umbrellaStand);
 }
