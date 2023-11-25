7c6119507fa74cd5910a5697ee53934f0414671a
yonding <uwrgoy7584@khu.ac.kr>
2023-11-25 20:04:30 +0900

    Feat: 푸시 알림 기록 저장하기

--- src/main/java/shop/mulmagi/app/service/impl/NotificationServiceImpl.java
+++ src/main/java/shop/mulmagi/app/service/impl/NotificationServiceImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.mulmagi.app.converter.NotificationConverter;
 import shop.mulmagi.app.domain.Notification;
+import shop.mulmagi.app.domain.User;
+import shop.mulmagi.app.domain.enums.NotificationType;
 import shop.mulmagi.app.repository.NotificationRepository;
 import shop.mulmagi.app.service.NotificationService;
 import shop.mulmagi.app.web.dto.NotificationResponseDto;
@@ -18,9 +20,25 @@ public class NotificationServiceImpl implements NotificationService {
     private final NotificationRepository notificationRepository;
     private final NotificationConverter notificationConverter;
     @Override
-    public List<NotificationResponseDto.NotificationHistoryDto> getNotificationHistory(Long id){
-        List<Notification> notificationList = notificationRepository.findByUserId(id);
+    public List<NotificationResponseDto.NotificationHistoryDto> getNotificationHistory(Long userId){
+        List<Notification> notificationList = notificationRepository.findByUserId(userId);
 
         return notificationConverter.toNotificationHistoryDtoList(notificationList);
+    }
+
+    @Override
+    public void sendAndSaveNotification(User user, NotificationType type, String mainContent, String subContent){
+        // 푸시 알림 보내기
+
+        // 알림 내역 저장
+        Notification notification =
+                Notification.builder()
+                .user(user)
+                .type(type)
+                .mainContent(mainContent)
+                .subContent(subContent)
+                .build();
+
+        notificationRepository.save(notification);
     }
 }
