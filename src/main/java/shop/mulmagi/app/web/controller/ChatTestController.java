0e80118bbf79a906b5371b88bcf9a3c6dc83ea1f
Yuseunggeun <yuu3730@naver.com>
2023-12-01 03:17:15 +0900

    Feat: stomp 테스트 페이지

---
+++ src/main/java/shop/mulmagi/app/web/controller/ChatTestController.java
@@ -0,0 +1,26 @@
+package shop.mulmagi.app.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.swagger.annotations.ApiOperation;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatTestController {
+
+	@ApiOperation(value = "test")
+	@GetMapping("/list")
+	public String chatList(Model model){
+		return "/chat/room";
+	}
+
+	@ApiOperation(value = "test")
+	@GetMapping("/room/enter/{roomId}")
+	public String chatList(Model model, @PathVariable String roomId){
+		return "/chat/roomdetail";
+	}
+}
