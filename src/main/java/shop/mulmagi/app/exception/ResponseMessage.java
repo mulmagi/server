ab2d36d8f63c7e29e70fa31ba728b65d9a656075
yonding <uwrgoy7584@khu.ac.kr>
2023-12-04 03:29:30 +0900

    Feat: notification 응답 메세지 수정 및 추가

--- src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
+++ src/main/java/shop/mulmagi/app/exception/ResponseMessage.java
@@ -15,7 +15,8 @@ public class ResponseMessage {
     public static final String PAYMENT_HISTORY_READ_SUCCESS = "포인트 내역 불러오기 성공";
   
     //notification
-    public static final String NOTIFICATION_HISTORY_READ_SUCCESS = "알림 내역 불러오기 성공";
+    public static final String NOTIFICATION_HISTORY_ALL_READ_SUCCESS = "전체 알림 내역 불러오기 성공";
+    public static final String NOTIFICATION_HISTORY_RENTAL_READ_SUCCESS = "대여/반납/연체 알림 내역 불러오기 성공";
     public static final String NOTIFICATION_ALLOW_SUCCESS = "푸시 알림 허용하기 성공";
     public static final String NOTIFICATION_DENY_SUCCESS = "푸시 알림 거부하기 성공";
 
