7533928b39a5ee5feacc248615e4dadbf3353567
yonding <uwrgoy7584@khu.ac.kr>
2023-12-04 03:48:18 +0900

    Feat: 알림 내역을 가까운 시간 순으로 정렬

--- src/main/java/shop/mulmagi/app/repository/NotificationRepository.java
+++ src/main/java/shop/mulmagi/app/repository/NotificationRepository.java
@@ -12,15 +12,18 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     List<Notification> findByUserId(Long id);
 
     @Query("SELECT n FROM Notification n " +
-            "WHERE n.user = :user AND n.type IN :rentalList")
+            "WHERE n.user = :user AND n.type IN :rentalList " +
+            "ORDER BY n.createdAt DESC")
     List<Notification> findRentalByUserId(User user, List<NotificationType> rentalList);
 
     @Query("SELECT n FROM Notification n " +
-            "WHERE n.user = :user AND n.type IN :pointList")
+            "WHERE n.user = :user AND n.type IN :pointList " +
+            "ORDER BY n.createdAt DESC")
     List<Notification> findPointByUserId(User user, List<NotificationType> pointList);
 
     @Query("SELECT n FROM Notification n " +
-            "WHERE n.user = :user AND n.type IN :etcList")
+            "WHERE n.user = :user AND n.type IN :etcList " +
+            "ORDER BY n.createdAt DESC")
     List<Notification> findEtcByUserId(User user, List<NotificationType> etcList);
 
 }
