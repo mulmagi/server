d3f281420d34432b03abaad73b34006d745c7bb3
yonding <uwrgoy7584@khu.ac.kr>
2023-12-04 11:57:29 +0900

    Test: VerifyIamportDto 테스트 예시 추가

--- src/main/java/shop/mulmagi/app/web/dto/PaymentRequestDto.java
+++ src/main/java/shop/mulmagi/app/web/dto/PaymentRequestDto.java
@@ -1,6 +1,7 @@
 package shop.mulmagi.app.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiParam;
 import lombok.Getter;
 
@@ -9,10 +10,13 @@ public class PaymentRequestDto {
     public static class VerifyIamportDto{
         @JsonProperty("imp_uid")
         @ApiParam(name = "imp_uid", value = "impUid 입력", required = true)
+        @ApiModelProperty(example = "imp_04252211")
         private String impUid;
         @ApiParam(name = "amount", value = "amount 입력", required = true)
+        @ApiModelProperty(example = "10000")
         private Integer amount;
-        @ApiParam(name = "mehod", value = "method 입력", required = true)
+        @ApiParam(name = "method", value = "method 입력", required = true)
+        @ApiModelProperty(example = "KAKAOPAY")
         private String method;
     }
 }
