190a23ec158bb9d3fc57cbbc0648d35b4f71889d
Yuseunggeun <yuu3730@naver.com>
2023-12-07 10:20:13 +0900

    Refactor: LocalDateTime JSON format 설정

--- src/main/java/shop/mulmagi/app/web/dto/MessageDto.java
+++ src/main/java/shop/mulmagi/app/web/dto/MessageDto.java
@@ -2,6 +2,7 @@ package shop.mulmagi.app.web.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -28,5 +29,6 @@ public class MessageDto {
 	private Boolean isAdmin;
 	@JsonSerialize(using = LocalDateTimeSerializer.class)
 	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 	private LocalDateTime createdAt;
 }
--- src/main/java/shop/mulmagi/app/web/dto/MessageResponseDto.java
+++ src/main/java/shop/mulmagi/app/web/dto/MessageResponseDto.java
@@ -3,6 +3,7 @@ package shop.mulmagi.app.web.dto;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -29,6 +30,7 @@ public class MessageResponseDto {
 		private Boolean isAdmin;
 		@JsonSerialize(using = LocalDateTimeSerializer.class)
 		@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+		@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
 		private LocalDateTime createdAt;
 	}
 }
