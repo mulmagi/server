74823c163e6cbff8a881cb882ea92b6b4ec4a792
Yuseunggeun <yuu3730@naver.com>
2023-12-20 03:29:04 +0900

    Refactor: POST 방식 채팅 return 추가

--- src/main/java/shop/mulmagi/app/web/controller/MessageController.java
+++ src/main/java/shop/mulmagi/app/web/controller/MessageController.java
@@ -2,32 +2,37 @@ package shop.mulmagi.app.web.controller;
 
 import static shop.mulmagi.app.domain.enums.MessageType.*;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import shop.mulmagi.app.service.impl.ChatServiceImpl;
-import shop.mulmagi.app.service.impl.S3UploadServiceImpl;
+import shop.mulmagi.app.exception.CustomExceptions;
+import shop.mulmagi.app.exception.ResponseMessage;
+import shop.mulmagi.app.exception.StatusCode;
+import shop.mulmagi.app.service.ChatService;
+import shop.mulmagi.app.service.S3UploadService;
 import shop.mulmagi.app.web.controller.base.BaseController;
 import shop.mulmagi.app.web.dto.MessageRequestDto;
 import shop.mulmagi.app.web.dto.MessageResponseDto;
+import shop.mulmagi.app.web.dto.base.DefaultRes;
 
 @Api(tags = "메시지 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class MessageController extends BaseController {
 	private final SimpMessageSendingOperations sendingOperations;
-	private final ChatServiceImpl chatService;
-	private final S3UploadServiceImpl s3UploadService;
+	private final ChatService chatService;
+	private final S3UploadService s3UploadService;
 
-	@MessageMapping("/chat/message")
+	@MessageMapping("/chat/message") //stomp
 	public void sendTextMessage(MessageRequestDto.TextMessageDto messageDto) {
 		//logger.info(messageDto.getContents());
 		MessageResponseDto.MessageDto messageRes = chatService.getMessage(messageDto);
@@ -40,29 +45,39 @@ public class MessageController extends BaseController {
 
 	@ApiOperation(value = "POST로 채팅보내기 test API")
 	@PostMapping("/chat/message/test")
-	public void sendTextMessageTest(@ModelAttribute MessageRequestDto.TextMessageDto request) {
-		//logger.info(messageDto.getContents());
-		MessageResponseDto.MessageDto messageRes = chatService.getMessage(request);
-		if(messageRes.getType().equals(ENTER) && !messageRes.getIsAdmin()){
-			messageRes.setContents(messageRes.getUserId().toString() + "님이 입장하였습니다");
+	public ResponseEntity sendTextMessageTest(@ModelAttribute MessageRequestDto.TextMessageDto request) {
+		try {
+			//logger.info(messageDto.getContents());
+			MessageResponseDto.MessageDto messageRes = chatService.getMessage(request);
+			if (messageRes.getType().equals(ENTER) && !messageRes.getIsAdmin()) {
+				messageRes.setContents(messageRes.getUserId().toString() + "님이 입장하였습니다");
+			}
+			messageRes = chatService.saveMessage(messageRes); //id와 createdAt 가져옴
+			sendingOperations.convertAndSend("/topic/chat/room/" + messageRes.getUserId().toString(), messageRes);
+			return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.POST_MESSAGE_SUCCESS, messageRes), HttpStatus.OK);
+		} catch (CustomExceptions.Exception e) {
+			return handleApiException(e, HttpStatus.BAD_REQUEST);
 		}
-		messageRes = chatService.saveMessage(messageRes); //id와 createdAt 가져옴
-		sendingOperations.convertAndSend("/topic/chat/room/" + messageRes.getUserId().toString(), messageRes);
 	}
 
 	//이미지 채팅 전송
 	@ApiOperation(value = "이미지 메시지 전송하기 API")
 	@PostMapping("/chat/message")
-	public void sendImgMessage(@ModelAttribute MessageRequestDto.ImgMessageDto request) {
-		logger.info(request.getUserId().toString());
-		MultipartFile img = request.getImg();
-		String imgUrl = "";
-		if(img != null && !img.isEmpty()) {
-			imgUrl = s3UploadService.uploadAWSChatImg(img);
-		}
-		MessageResponseDto.MessageDto messageRes = chatService.getMessage(request, imgUrl);
+	public ResponseEntity sendImgMessage(@ModelAttribute MessageRequestDto.ImgMessageDto request) {
+		try {
+			MultipartFile img = request.getImg();
+			String imgUrl = "";
+			if (img != null && !img.isEmpty()) {
+				imgUrl = s3UploadService.uploadAWSChatImg(img);
+			}
+			MessageResponseDto.MessageDto messageRes = chatService.getMessage(request, imgUrl);
 
-		messageRes = chatService.saveMessage(messageRes);
-		sendingOperations.convertAndSend("/topic/chat/room/" + messageRes.getUserId().toString(), messageRes);
+			messageRes = chatService.saveMessage(messageRes);
+			sendingOperations.convertAndSend("/topic/chat/room/" + messageRes.getUserId().toString(), messageRes);
+			return new ResponseEntity(DefaultRes.res(StatusCode.OK, ResponseMessage.POST_IMAGE_SUCCESS, messageRes), HttpStatus.OK);
+		}
+		catch (CustomExceptions.Exception e) {
+			return handleApiException(e, HttpStatus.BAD_REQUEST);
+		}
 	}
 }
